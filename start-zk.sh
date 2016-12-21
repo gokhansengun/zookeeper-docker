7dc3aede1f6ef03ff79412bfe1810efad4b1dfb6
Gökhan Şengün <gokhansengun@gmail.com>
2016-12-21 08:49:09 +0300

    Add hostname to the myid file for clustering

--- start-zk.sh
+++ start-zk.sh
@@ -1,4 +1,7 @@
 sed -i -r 's|#(log4j.appender.ROLLINGFILE.MaxBackupIndex.*)|\1|g' $ZK_HOME/conf/log4j.properties
 sed -i -r 's|#autopurge|autopurge|g' $ZK_HOME/conf/zoo.cfg
 
+## put the hostname into myid file, good for clusters
+hostname > /opt/zookeeper-3.4.9/data/myid
+
 /opt/zookeeper-3.4.9/bin/zkServer.sh start-foreground
